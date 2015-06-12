d787e07e89307bb6dab4c0d715bc2200bc2a852b
astaric <anze.staric@gmail.com>
2015-06-12 15:23:31 +0200

    Corpus extends Table.

--- orangecontrib/text/corpus.py
+++ orangecontrib/text/corpus.py
@@ -1,6 +1,9 @@
 import os
 import codecs
 
+import numpy as np
+from Orange.data import Domain, StringVariable, Table, DiscreteVariable, ContinuousVariable
+
 
 def get_sample_corpora_dir():
     path = os.path.dirname(__file__)
@@ -8,24 +11,60 @@ def get_sample_corpora_dir():
     return os.path.realpath(dir)
 
 
-class Corpus:
+class Corpus(Table):
     """
         Internal class for storing a corpus of orangecontrib.text.corpus.Document.
     """
+
+    def __new__(cls, *args, **kwargs):
+        """Bypass Table.__new__."""
+        return object.__new__(Corpus)
+
     def __init__(self, documents, metadata=None):
         self.documents = documents
 
+        metadata2 = dict()
+        for md in metadata:
+            for key in md.keys():
+                metadata2.setdefault(key, set()).add(md[key])
+
+        meta_vars = []
+        for key in sorted(metadata2.keys()):
+            if len(metadata2[key]) < 21:
+                meta_vars.append(DiscreteVariable(key, values=list(metadata2[key])))
+            else:
+                meta_vars.append(StringVariable(key))
+
+        metas = [[None] * len(meta_vars) for x in range(len(documents))]
+        for i, md in enumerate(metadata):
+            for j, var in enumerate(meta_vars):
+                if var.name in md:
+                    metas[i][j] = var.to_val(md[var.name])
+
+        text = np.array(documents).reshape(len(documents), 1)
+        metas = np.array(metas, dtype=object)
+
+        self.metas = np.hstack((text, metas))
+        self.X = self._Y = self.W = np.zeros((len(documents), 0))
+        self.domain = Domain([], metas=[StringVariable('Text')] + meta_vars)
+        Table._init_ids(self)
+
+    def _create_metas(self, metadata):
+        return np.zeros((len(self), 0))
+
     def get_number_of_categories(self):
-        return len(set([d.category for d in self.documents]))
+        return len(self.domain['category'].values)
 
     @classmethod
     def from_file(cls, filename):
         documents = []
+        metadata = []
         with codecs.open(filename, 'r', 'utf-8') as f:
             for line in f:
                 category, text = line.strip().split("\t")
-                documents.append(Document(text, category))
-        return cls(documents)
+                documents.append(text)
+                metadata.append(dict(category=category))
+        return cls(documents, metadata)
 
     def __len__(self):
         return len(self.documents)
