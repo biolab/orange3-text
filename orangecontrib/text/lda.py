ec19511f8bbe60e1ea963de5a3f0990068a4bc80
nikicc <niko.colneric@gmail.com>
2015-06-16 17:21:45 +0200

    LDA topics in meta_attributes, exp notation.

--- orangecontrib/text/lda.py
+++ orangecontrib/text/lda.py
@@ -2,7 +2,7 @@ import numpy as np
 from gensim import corpora, models, matutils
 
 from Orange.data.table import Table
-from Orange.data.domain import Domain, ContinuousVariable, DiscreteVariable
+from Orange.data.domain import Domain, ContinuousVariable, StringVariable
 
 def chunk_list(l, num):
     num = min(len(l), num)
@@ -40,12 +40,17 @@ class LDA:
 
         lda = models.LdaModel(id2word=dictionary, num_topics=self.num_topics)
         done = 0
-        for i, part in enumerate(chunk_list(corpus, 90)):
+        for i, part in enumerate(chunk_list(corpus, 95)):
             lda.update(part)
             done += len(part)
-            callback(90.0*done/len(corpus))
-        corpus = lda[corpus]
+            callback(95.0*done/len(corpus))
 
+        corpus = lda[corpus]
+        topics = lda.show_topics(num_topics=-1, num_words=3, formatted=False)
+        names = [', '.join([i[1] for i in t]) for t in topics]
+        names = ['Topic{} ({})'.format(i, n) for i, n in enumerate(names, 1)]
+
+        self.topic_names = names
         self.corpus = corpus
         self.lda = lda
 
@@ -60,9 +65,7 @@ class LDA:
                                        num_terms=self.num_topics).T
 
         # Generate the new table.
-        topics = self.lda.show_topics(num_topics=-1, num_words=3, formatted=False)
-        names = [', '.join([i[1] for i in t]) for t in topics]
-        attr = [ContinuousVariable("T{} ({}, ...)".format(i, n)) for i, n in enumerate(names, 1)]
+        attr = [ContinuousVariable(n) for n in self.topic_names]
         domain = Domain(attr,
                         corp_in.domain.class_vars,
                         metas=corp_in.domain.metas)
@@ -79,26 +82,23 @@ class LDA:
         :param lda: gensim LDA model.
         :return: `Orange.data.table.Table`.
         """
-
         topics = self.lda.show_topics(num_topics=-1, num_words=-1, formatted=False)
-        ntopics = len(topics)
-        nwords = max([len(it) for it in topics])
+        num_topics = len(topics)
+        num_words = max([len(it) for it in topics])
 
-        data = np.zeros((nwords, 2*ntopics), dtype=object)
+        data = np.zeros((num_words, 2*num_topics), dtype=object)
 
         for i, topic in enumerate(topics):
             data[:, 2*i] = [item[1] for item in topic]
-            data[:, 2*i+1] = ['{:.3e}'.format(item[0]) for item in topic]
+            data[:, 2*i+1] = [item[0] for item in topic]
 
-        attr = []
-        for i in range(ntopics):
-            attr.append(DiscreteVariable("T{}".format(i+1), values=list(data[:, 2*i])))
-            attr.append(ContinuousVariable("T{}_weights".format(i+1)))
-        domain = Domain(attr)
+        metas = []
+        for i in range(num_topics):
+            metas.append(StringVariable(self.topic_names[i]))
+            metas.append(ContinuousVariable("Topic{}_weights".format(i+1)))
+            metas[-1]._out_format = '%.2e'
 
-        for t in range(data.shape[1]):
-            for w in range(data.shape[0]):
-                if t % 2 == 0:
-                    data[w, t] = attr[t].to_val(data[w, t])
-
-        return Table.from_numpy(domain, data)
+        domain = Domain([], metas=metas)
+        return Table.from_numpy(domain,
+                                X=np.zeros((num_words, 0)),
+                                metas=data)
