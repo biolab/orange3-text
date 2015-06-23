da7273ece5274a31f6da7cfce62b9f53b75f0e7f
nikicc <niko.colneric@gmail.com>
2015-06-23 17:35:00 +0200

    Word enrichment info box, disabling control area when calculating.

--- orangecontrib/text/widgets/OWWordEnrichment.py
+++ orangecontrib/text/widgets/OWWordEnrichment.py
@@ -1,4 +1,5 @@
 from PyQt4 import QtGui
+import numpy as np
 
 from Orange.widgets import gui
 from Orange.widgets.settings import Setting
@@ -22,8 +23,8 @@ class OWWordEnrichment(OWWidget):
     # Settings
     filter_by_p = Setting(False)
     filter_p_value = Setting(0.01)
-    filter_by_fdr = Setting(False)
-    filter_fdr_value = Setting(0.01)
+    filter_by_fdr = Setting(True)
+    filter_fdr_value = Setting(0.2)
 
     def __init__(self):
         super().__init__()
@@ -35,27 +36,36 @@ class OWWordEnrichment(OWWidget):
         self.p_values = []
         self.fdr_values = []
 
+        # Info section
+        fbox = gui.widgetBox(self.controlArea, "Info")
+        self.info_all = gui.label(fbox, self, 'Cluster words:')
+        self.info_sel = gui.label(fbox, self, 'Selected words:')
+        self.info_fil = gui.label(fbox, self, 'After filtering:')
+
         # Filtering settings
-        fbox = gui.widgetBox(self.controlArea, "Filter words")
+        fbox = gui.widgetBox(self.controlArea, "Filter")
+        hbox = gui.widgetBox(fbox, orientation=0)
 
-        gui.checkBox(fbox, self, "filter_by_p", "p-value",
-                     callback=self.filter_and_display,
-                     tooltip="Filter by word p-value")
-        gui.doubleSpin(gui.indentedBox(fbox), self, 'filter_p_value',
-                       1e-4, 1, step=1e-4,  label='p:', labelWidth=15,
-                       callback=self.filter_and_display,
-                       callbackOnReturn=True,
-                       tooltip="Max p-value for word")
+        self.chb_p = gui.checkBox(hbox, self, "filter_by_p", "p-value",
+                                  callback=self.filter_and_display,
+                                  tooltip="Filter by word p-value")
+        self.spin_p = gui.doubleSpin(hbox, self, 'filter_p_value',
+                                     1e-4, 1, step=1e-4, labelWidth=15,
+                                     callback=self.filter_and_display,
+                                     callbackOnReturn=True,
+                                     tooltip="Max p-value for word")
+        self.spin_p.setEnabled(self.filter_by_p)
 
-        gui.checkBox(fbox, self, "filter_by_fdr", "FDR",
-                     callback=self.filter_and_display,
-                     tooltip="Filter by word FDR")
-        gui.doubleSpin(gui.indentedBox(fbox), self, 'filter_fdr_value',
-                       1e-4, 1, step=1e-4,  label='p:', labelWidth=15,
-                       callback=self.filter_and_display,
-                       callbackOnReturn=True,
-                       tooltip="Max p-value for word")
-
+        hbox = gui.widgetBox(fbox, orientation=0)
+        self.chb_fdr = gui.checkBox(hbox, self, "filter_by_fdr", "FDR",
+                                    callback=self.filter_and_display,
+                                    tooltip="Filter by word FDR")
+        self.spin_fdr = gui.doubleSpin(hbox, self, 'filter_fdr_value',
+                                       1e-4, 1, step=1e-4, labelWidth=15,
+                                       callback=self.filter_and_display,
+                                       callbackOnReturn=True,
+                                       tooltip="Max p-value for word")
+        self.spin_fdr.setEnabled(self.filter_by_fdr)
         gui.rubber(self.controlArea)
 
         # Word's list view
@@ -65,7 +75,7 @@ class OWWordEnrichment(OWWidget):
         self.sig_words.setHeaderLabels(self.cols)
         self.sig_words.setSortingEnabled(True)
         self.sig_words.setSelectionMode(QtGui.QTreeView.ExtendedSelection)
-        self.sig_words.sortByColumn(1, 0)   # 0 is ascending order
+        self.sig_words.sortByColumn(2, 0)   # 0 is ascending order
         for i in range(len(self.cols)):
             self.sig_words.resizeColumnToContents(i)
         self.mainArea.layout().addWidget(self.sig_words)
@@ -93,20 +103,40 @@ class OWWordEnrichment(OWWidget):
 
     def clear(self):
         self.sig_words.clear()
-        self.sig_words.sortByColumn(1, 0)   # 0 is ascending order
+        self.info_all.setText('Cluster words:')
+        self.info_sel.setText('Selected words:')
+        self.info_fil.setText('After filtering:')
 
-    def filter_and_display(self, resize_columns=False):
+    def filter_enabled(self, b):
+        self.chb_p.setEnabled(b)
+        self.chb_fdr.setEnabled(b)
+        self.spin_p.setEnabled(b)
+        self.spin_fdr.setEnabled(b)
+
+    def filter_and_display(self):
+        self.spin_p.setEnabled(self.filter_by_p)
+        self.spin_fdr.setEnabled(self.filter_by_fdr)
         self.sig_words.clear()
+        count = 0
         if self.words:
             for word, pval, fval in zip(self.words, self.p_values, self.fdr_values):
                 if (not self.filter_by_p or pval <= self.filter_p_value) and \
                         (not self.filter_by_fdr or fval <= self.filter_fdr_value):
                     it = EATreeWidgetItem(word, pval, fval, self.sig_words)
                     self.sig_words.addTopLevelItem(it)
+                    count += 1
 
-        if resize_columns:
-            for i in range(len(self.cols)):
-                self.sig_words.resizeColumnToContents(i)
+        for i in range(len(self.cols)):
+            self.sig_words.resizeColumnToContents(i)
+
+        self.info_all.setText('Cluster words: {}'.format(len(self.selected_data.domain.attributes)))
+        self.info_sel.setText('Selected words: {}'.format(np.count_nonzero(np.sum(self.selected_data.X, axis=0))))
+        if not self.filter_by_p and not self.filter_by_fdr:
+            self.info_fil.setText('After filtering:')
+            self.info_fil.setEnabled(False)
+        else:
+            self.info_fil.setEnabled(True)
+            self.info_fil.setText('After filtering: {}'.format(count))
 
     def progress(self, p):
         self.progressBarSet(p)
@@ -114,12 +144,16 @@ class OWWordEnrichment(OWWidget):
     def apply(self):
         if self.selected_data.X.size > 0:
             self.progressBarInit()
+            self.clear()
+            self.filter_enabled(False)
+
             self.words = [i.name for i in self.selected_data.domain.attributes]
             self.p_values = hypergeom_p_values(self.selected_data.X,
                                                self.other_data.X,
                                                callback=self.progress)
             self.fdr_values = false_discovery_rate(self.p_values)
-            self.filter_and_display(resize_columns=True)
+            self.filter_and_display()
+            self.filter_enabled(True)
             self.progressBarFinished()
         else:
             self.clear()
