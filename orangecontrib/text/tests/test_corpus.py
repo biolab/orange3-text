17e77a38cb7b0cde2827203cfea9012470b1bfa6
nikicc <niko.colneric@gmail.com>
2015-08-21 11:19:49 +0200

    tests_corpus, tests_preprocess: Updated tests to tab
    Corpuses are not loaded any more from txt files but as tab files.
    Updated the tests and commented out the preprocessing test for which
    the code is not yet implemented.

--- orangecontrib/text/tests/test_corpus.py
+++ orangecontrib/text/tests/test_corpus.py
@@ -8,15 +8,15 @@ DATASET_PATH = os.path.join(os.path.dirname(__file__), '..', 'datasets')
 
 class CorpusTests(unittest.TestCase):
     def test_corpus_from_file(self):
-        c = Corpus.from_file(os.path.join(DATASET_PATH, 'bookexcerpts.txt'))
+        c = Corpus.from_file(os.path.join(DATASET_PATH, 'bookexcerpts.tab'))
         self.assertEqual(len(c), 140)
 
-        self.assertEqual(len(c.domain), 0)
-        self.assertEqual(len(c.domain.metas), 2)
-        self.assertEqual(c.metas.shape, (140, 2))
+        self.assertEqual(len(c.domain), 1)
+        self.assertEqual(len(c.domain.metas), 1)
+        self.assertEqual(c.metas.shape, (140, 1))
 
     def test_corpus_from_file_just_text(self):
-        c = Corpus.from_file(os.path.join(DATASET_PATH, 'deerwester.txt'))
+        c = Corpus.from_file(os.path.join(DATASET_PATH, 'deerwester.tab'))
 
         self.assertEqual(len(c), 9)
 
