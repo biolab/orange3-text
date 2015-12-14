9fac481eed028467cf74ca0c9c689172d9c6a058
nikicc <niko.colneric@gmail.com>
2015-12-14 16:58:51 +0100

    setup&readme: removed dependency link and added a warning
    The dependency links only work when pip is called with a flag to enable the dependency links, so this never worked. We will wait for a new smart_open release. Since for now the instalation through Orange does not work I added a warning to the package description.

--- setup.py
+++ setup.py
@@ -70,11 +70,7 @@ if __name__ == '__main__':
         download_url=DOWNLOAD_URL,
         packages=find_packages(),
         include_package_data=True,
-        install_requires=
-            INSTALL_REQUIRES + ('smart_open>=1.3.0',),
-        dependency_links=(
-            'git+https://github.com/piskvorky/smart_open.git@1fe93a2#egg=smart_open-1.3.0',
-        ),
+        install_requires=INSTALL_REQUIRES,
         entry_points=ENTRY_POINTS,
         keywords=KEYWORDS,
         namespace_packages=['orangecontrib'],
