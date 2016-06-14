19d44bcbe0537fda609f2ac7075fe8161365b76f
alexey <suharevichalexey@gmail.com>
2016-06-14 03:44:28 +0300

    Preprocess: ensure NLTK data is present.

--- orangecontrib/text/preprocess/__init__.py
+++ orangecontrib/text/preprocess/__init__.py
@@ -1,8 +1,14 @@
+# Ensure NLTK data is present
+import nltk
+NLTK_DATA = ["wordnet",  "stopwords",  "punkt"]
+nltk.download(NLTK_DATA, quiet=True)
+
 from .filter import *
 from .normalize import *
 from .tokenize import *
 from .transform import *
 from .preprocess import *
+
 
 
 base_preprocessor = Preprocessor(transformers=[LowercaseTransformer()],
