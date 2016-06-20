d4f78ff4263db40b786631f3b67c5dec26f24ef2
alexey <suharevichalexey@gmail.com>
2016-06-20 12:06:11 +0300

    Vectorizer: setup CountVectorizer, TfidfVectorizer and Simhash classes

--- orangecontrib/text/bagofowords.py
+++ orangecontrib/text/bagofowords.py
@@ -1,53 +1,127 @@
-import warnings
-
+import nltk
 import numpy as np
+from simhash import Simhash
 
-from gensim import corpora, matutils
-from gensim.models.tfidfmodel import TfidfModel
+from sklearn.feature_extraction import text
+
+from orangecontrib.text.corpus import Corpus
 
 
-class BagOfWords():
+__all__ = ['CountVectorizer', 'TfidfVectorizer', 'SimhashVectorizer']
 
-    def __init__(self, progress_callback=None, error_callback=None):
-        self.progress_callback = progress_callback
-        self.error_callback = error_callback
-        self.dictionary = None
 
-    def __call__(self, corpus, use_tfidf=False):
-        if corpus is None:
-            raise ValueError(
-                    'Cannot compute Bag of Words without an input corpus.'
-            )
+class SklearnVectorizerMixin:
 
-        self.check_progress()  # Step 1
+    def fit(self, data, y=None):
+        if isinstance(data, Corpus):
+            return super().fit(data.tokens, y)
+        return super().fit(data, y)
 
-        self.dictionary = corpus.dictionary
+    def fit_transform(self, corpus, y=None):
+        if isinstance(corpus, Corpus):
+            matrix = super().fit_transform(corpus.tokens, y)
+            return self.__extend_corpus(corpus, matrix)
+        return super().fit_transform(corpus, y)
 
-        # Term frequencies.
-        bag_of_words = [self.dictionary.doc2bow(i) for i in corpus.tokens]
+    def transform(self, corpus):
+        if isinstance(corpus, Corpus):
+            matrix = super().transform(corpus.tokens)
+            return self.__extend_corpus(corpus, matrix)
+        return super().transform(corpus)
 
-        self.check_progress()  # Step 2
+    def __extend_corpus(self, corpus, matrix):
+        new_corpus = corpus.copy()
+        new_corpus.extend_attributes(matrix, sorted(self.vocabulary_.keys()),
+                                     var_attrs={'hidden': True})
+        return new_corpus
 
-        if use_tfidf:
-            tfidf_model = TfidfModel(bag_of_words)
-            bag_of_words = tfidf_model[bag_of_words]
 
-        self.check_progress()  # Step 3
+class CountVectorizer(SklearnVectorizerMixin, text.CountVectorizer):
+    name = 'Count Vectorizer'
 
-        X = matutils.corpus2csc(
-                bag_of_words,
-                num_terms=len(self.dictionary.keys()),
-                dtype=np.float64
-        ).T
+    def __init__(self, binary=False, ngram_range=(1, 1)):
+        super().__init__(input='content', encoding='utf-8',
+                         decode_error='strict', strip_accents=None,
+                         lowercase=False, preprocessor=lambda x: x, tokenizer=lambda x: x,
+                         stop_words=None, token_pattern=r"(?u)\b\w\w+\b",
+                         ngram_range=ngram_range, analyzer='word',
+                         max_df=1.0, min_df=1, max_features=None,
+                         vocabulary=None, binary=binary, dtype=np.int64)
 
-        bow_corpus = corpus.copy()
-        feats = [v for k, v in sorted(self.dictionary.items())]
-        bow_corpus.extend_attributes(X, feats, var_attrs={'hidden': True})
+    def report(self):
+        return (('N-gram range', self.ngram_range),
+                ('Binary', self.binary))
 
-        self.check_progress()  # Step 4
 
-        return bow_corpus
+class TfidfVectorizer(SklearnVectorizerMixin, text.TfidfVectorizer):
+    name = 'Tfidf Vectorizer'
 
-    def check_progress(self):
-        if self.progress_callback:
-            self.progress_callback()
+    def __init__(self, binary=False, norm='l2', use_idf=True, smooth_idf=True,
+                 sublinear_tf=False, ngram_range=(1, 1)):
+        super().__init__(input='content', encoding='utf-8',
+                         decode_error='strict', strip_accents=None, lowercase=False,
+                         preprocessor=lambda x: x, tokenizer=lambda x: x, analyzer='word',
+                         stop_words=None, token_pattern=r"(?u)\b\w\w+\b",
+                         ngram_range=ngram_range, max_df=1.0, min_df=1,
+                         max_features=None, vocabulary=None, binary=binary,
+                         dtype=np.int64, norm=norm, use_idf=use_idf, smooth_idf=smooth_idf,
+                         sublinear_tf=sublinear_tf)
+
+    def report(self):
+        return (('N-gram range', self.ngram_range),
+                ('Binary', self.binary),
+                ('Use idf', self.use_idf),
+                ('Smooth idf', self.smooth_idf),
+                ('Sublinear tf', self.sublinear_tf))
+
+
+class SimhashVectorizer:
+    name = "Simhash"
+    max_f = 1024
+
+    def __init__(self, shingle_len=10, f=64, hashfunc=None):
+        """
+        Args:
+            shingle_len(int): Length of a shingle.
+            f(int): Length of a document fingerprints
+            hashfunc(callable): A function that accepts a string and returns
+                a unsigned integer
+        """
+        self.f = f
+        self._bin_format = '{:0%db}' % self.f
+        self.hashfunc = hashfunc
+        self.ngram_len = shingle_len
+
+    @staticmethod
+    def get_shingles(tokens, n):
+        return map(lambda x: ''.join(x), nltk.ngrams(tokens, n))
+
+    def compute_hash(self, tokens):
+        return Simhash(self.get_shingles(tokens, self.ngram_len), f=self.f, hashfunc=self.hashfunc).value
+
+    def int2binarray(self, num):
+        return [int(x) for x in self._bin_format.format(num)]
+
+    def transform(self, corpus):
+        """ Computes simhash values from the given corpus
+        and creates a new one with a simhash attribute.
+
+        Args:
+            corpus (Corpus): a corpus with tokens.
+
+        Returns:
+            Corpus with `simhash` variable
+        """
+
+        X = np.array([self.int2binarray(self.compute_hash(doc)) for doc in corpus.tokens])
+        corpus = corpus.copy()
+        corpus.extend_attributes(X, ('simhash_{}'.format(i) for i in range(self.f)),
+                                 var_attrs={'hidden': True})
+        return corpus
+
+    def fit_transform(self, corpus):
+        return self.transform(corpus)
+
+    def report(self):
+        return (('Hash length', self.f),
+                ('Shingle length', self.ngram_len))
--- orangecontrib/text/tests/test_bag_of_words.py
+++ orangecontrib/text/tests/test_bag_of_words.py
@@ -1,41 +1,77 @@
 import unittest
 
-from orangecontrib.text.bagofowords import BagOfWords
+from orangecontrib.text.bagofowords import CountVectorizer, TfidfVectorizer, SimhashVectorizer
 from orangecontrib.text.corpus import Corpus
 
 
-class BagOfWordsTests(unittest.TestCase):
+class VectorizerTest:
+    Vectorizer = NotImplemented
 
-    def progress_callback(self):
-        self.progress_callbacks += 1
-        return
+    def test_report(self):
+        vect = self.Vectorizer()
+        self.assertGreater(len(vect.report()), 1)
 
-    def error_callback(self, *args):
-        self.error_callbacks += 1
-        return
+
+class SklearnVectorizerTests(VectorizerTest):
+    def test_fit_transform(self):
+        vect = self.Vectorizer()
+        corpus = Corpus.from_file('deerwester')
+
+        result = vect.fit_transform(corpus)
+        self.assertIsInstance(result, Corpus)
+        self.assertEqual(len(result.domain), 43)
+
+    def test_transform(self):
+        vect = self.Vectorizer()
+        corpus = Corpus.from_file('deerwester')
+
+        vect.fit(corpus)
+        result = vect.transform(corpus)
+        self.assertIsInstance(result, Corpus)
+        self.assertEqual(len(result.domain), 43)
+
+    def test_domain(self):
+        vect = self.Vectorizer()
+        corpus = Corpus.from_file('deerwester')
+
+        vect.fit(corpus)
+        result = vect.transform(corpus)
+        attrs = [attr.name for attr in result.domain.attributes]
+        self.assertEqual(attrs, sorted(attrs))
+
+        X = result.X.toarray()
+        for i in range(len(corpus)):
+            for contains, attr in zip(X[i], attrs):
+                if contains:
+                    self.assertIn(attr, corpus.tokens[i])
+
+    def test_ngrams(self):
+        vect = self.Vectorizer(ngram_range=(1, 3))
+        corpus = Corpus.from_file('deerwester')
+        result = vect.fit_transform(corpus)
+        attrs = [attr.name for attr in result.domain.attributes]
+        self.assertIn(corpus.tokens[0][1], attrs)
+        self.assertIn(' '.join(corpus.tokens[0][:2]), attrs)
+        self.assertIn(' '.join(corpus.tokens[0][:3]), attrs)
+
+
+class CountVectorizerTests(SklearnVectorizerTests, unittest.TestCase):
+    Vectorizer = CountVectorizer
+
+
+class TfidfVectorizerTests(SklearnVectorizerTests, unittest.TestCase):
+    Vectorizer = TfidfVectorizer
+
+
+class TestSimhash(VectorizerTest, unittest.TestCase):
+    Vectorizer = SimhashVectorizer
 
     def setUp(self):
-        self.progress_callbacks = 0
-        self.error_callbacks = 0
+        self.corpus = Corpus.from_file('deerwester')
 
-        self.bow = BagOfWords(
-                progress_callback=self.progress_callback,
-                error_callback=self.error_callback,
-        )
-
-    def test_create_bow(self):
-        corpus = Corpus.from_file('deerwester')
-        bag_of_words = self.bow(corpus, use_tfidf=True)
-
-        self.assertIsNotNone(bag_of_words.X)
-        self.assertEqual(9, bag_of_words.X.shape[0])
-        self.assertEqual(42, bag_of_words.X.shape[1])
-        self.assertEqual(self.progress_callbacks, 4)
-        self.assertEqual(self.error_callbacks, 0)
-
-    def test_bow_exceptions(self):
-        self.assertRaises(
-            ValueError,
-            self.bow,
-            None
-        )
+    def test_transform(self):
+        vect = SimhashVectorizer(shingle_len=10, f=64)
+        result = vect.transform(self.corpus)
+        self.assertIsInstance(result, Corpus)
+        self.assertEqual(len(result), len(self.corpus))
+        self.assertEqual(result.X.shape, (len(self.corpus), 64))
