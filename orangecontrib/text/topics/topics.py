f2943252ecf8f9c5c9c34cf0988d38b255e7c023
alexey <suharevichalexey@gmail.com>
2016-07-16 11:51:34 +0300

    LsiModel: fix an issue when auto decreased amount of topics caused an error

--- orangecontrib/text/topics/topics.py
+++ orangecontrib/text/topics/topics.py
@@ -97,8 +97,10 @@ class GensimWrapper:
 
     def get_top_words_by_id(self, topic_id, num_of_words=10):
         topics = self._topics_words(num_of_words=num_of_words)
-        if topic_id >= len(topics):
+        if topic_id >= self.num_topics:
             raise ValueError("Too large topic ID.")
+        elif topic_id >= len(topics):
+            return []
         return topics[topic_id]
 
     def _topics_words(self, num_of_words):
