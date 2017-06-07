169f9285cff932f8b0e89ec9f38385fc3ec8c79f
nikicc <niko.colneric@gmail.com>
2017-06-07 10:30:52 +0200

    NLTK download to F:...

--- orangecontrib/text/misc/environ.py
+++ orangecontrib/text/misc/environ.py
@@ -6,6 +6,8 @@ from Orange.misc.environ import data_dir_base
 def nltk_data_dir():
     """ Location where the NLTK data is stored. """
     dir_ = os.path.join(data_dir_base(), 'Orange', 'nltk_data')
+    dir_ = "F:\\Orange"
     # make sure folder exists for ReadTheDocs
     os.makedirs(dir_, exist_ok=True)
+    # return dir_
     return dir_
