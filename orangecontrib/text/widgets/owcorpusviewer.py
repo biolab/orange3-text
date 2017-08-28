04c1249b098e6c163e5c438a56ba25d60c344173
nikicc <niko.colneric@gmail.com>
2017-08-28 13:37:04 +0200

    CorpusViewer: Break long urls

--- orangecontrib/text/widgets/owcorpusviewer.py
+++ orangecontrib/text/widgets/owcorpusviewer.py
@@ -245,6 +245,26 @@ class OWCorpusViewer(OWWidget):
             vertical-align: top;
             padding-right: 10px;
         }}
+        
+        .content {{
+            /* Adopted from https://css-tricks.com/snippets/css/prevent-long-urls-from-breaking-out-of-container/ */
+        
+            /* These are technically the same, but use both */
+            overflow-wrap: break-word;
+            word-wrap: break-word;
+        
+            -ms-word-break: break-all;
+            /* This is the dangerous one in WebKit, as it breaks things wherever */
+            word-break: break-all;
+            /* Instead use this non-standard one: */
+            word-break: break-word;
+        
+            /* Adds a hyphen where the word breaks, if supported (No Blink) */
+            -ms-hyphens: auto;
+            -moz-hyphens: auto;
+            -webkit-hyphens: auto;
+            hyphens: auto;
+        }}
 
         .token {{
             padding: 3px;
@@ -297,7 +317,7 @@ class OWCorpusViewer(OWWidget):
                 if is_image and value != '?':
                     value = '<img src="{}"></img>'.format(value)
                 html += '<tr><td class="variables"><strong>{}:</strong></td>' \
-                        '<td {}>{}</td></tr>'.format(
+                        '<td class="content" {}>{}</td></tr>'.format(
                     feature.name, mark, value)
 
             if self.show_tokens:
