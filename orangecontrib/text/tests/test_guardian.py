e70b824e6aef098b6deb9fecad3bbf7b3aac4ac7
robert <robert.cvitkovic@gmail.com>
2018-08-02 08:44:07 +0200

    tests: update guardian test

--- orangecontrib/text/tests/test_guardian.py
+++ orangecontrib/text/tests/test_guardian.py
@@ -2,6 +2,7 @@ import unittest
 import os
 
 from datetime import date, datetime
+from unittest import mock
 
 from orangecontrib.text import guardian
 
@@ -10,7 +11,9 @@ API_KEY = os.getenv('THE_GUARDIAN_API_KEY', 'test')
 
 
 class TestCredentials(unittest.TestCase):
-    def test_valid(self):
+    @mock.patch('requests.get')
+    def test_valid(self, mock_get):
+        mock_get().status_code = 200
         credentials = guardian.TheGuardianCredentials(API_KEY)
         self.assertTrue(credentials.valid)
 
@@ -19,15 +22,26 @@ class TestCredentials(unittest.TestCase):
         self.assertEquals(credentials, credentials)
 
 
+def skip_limit_exceeded(fun):
+    def wrapper(*args, **kwargs):
+        try:
+            fun(*args, **kwargs)
+        except guardian.APILimitError as err:
+            raise unittest.SkipTest(str(err))
+    return wrapper
+
+
 class TestGuardian(unittest.TestCase):
     def setUp(self):
         c = guardian.TheGuardianCredentials(API_KEY)
         self.api = guardian.TheGuardianAPI(c)
 
+    @skip_limit_exceeded
     def test_search(self):
         corp = self.api.search('Slovenia', max_documents=20)
         self.assertEqual(len(corp), 20)
 
+    @skip_limit_exceeded
     def test_search_from_to_date(self):
         from_date = date(2016, 12, 1)
         to_date = date(2016, 12, 31)
@@ -41,13 +55,53 @@ class TestGuardian(unittest.TestCase):
             self.assertGreaterEqual(dt.date(), from_date)
             self.assertLessEqual(dt.date(), to_date)
 
+    @skip_limit_exceeded
     def test_breaking(self):
         self.api.should_break = lambda: True
         corp = self.api.search('Slovenia', max_documents=20)
         self.assertEqual(len(corp), 10)
         self.api.should_break = lambda: False
 
+    @skip_limit_exceeded
     def test_accumulate(self):
         self.api.search('Slovenia', max_documents=10, accumulate=True)
         corp = self.api.search('Ljubljana', max_documents=10, accumulate=True)
         self.assertEqual(len(corp), 20)
+
+    @mock.patch('requests.get')
+    def test_api_limit_error(self, mock_get):
+        mock_get().status_code = 429
+        self.assertRaises(guardian.APILimitError, self.api.search, 'Slovenia')
+
+    @mock.patch('requests.get')
+    def test_search_mock_data(self, mock_get):
+        mock_get().text = """
+        {
+          "response": {
+            "pages": 1,
+            "results": [
+              {
+                "type": "article",
+                "sectionName": "World news",
+                "webPublicationDate": "2018-07-05T23:27:25Z",
+                "webUrl": "https://www.theguardian.com/world/2018/jul/06",
+                "fields": {
+                  "headline": "Rohingya refugees reject UN-Myanmar repatriati",
+                  "trailText": "Leaders say agreement does not address concer",
+                  "body": "<p><strong><strong><strong></strong></strong></str",
+                  "wordcount": "512",
+                  "lang": "en",
+                  "bodyText": "Rohingya community leaders have rejected an."
+                },
+                "tags": [
+                  {
+                    "webTitle": "Myanmar"
+                  }
+                ]
+              }
+            ]
+          }
+        }
+        """
+        corp = self.api.search('Slovenia', max_documents=1)
+        self.assertEqual(len(corp), 1)
