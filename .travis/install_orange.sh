4027f0b9882220ff01243223d22ff6b5e1357fe9
Primož Godec <p.godec9@gmail.com>
2020-09-01 11:31:07 +0200

    Travis: one test case with orange3-network (#520)

--- .travis/install_orange.sh
+++ .travis/install_orange.sh
@@ -1,20 +1,21 @@
 # CommonMark changed their interface in 0.8.0. Because older
-# Orange is imcompatible with newer CommonMark, 
+# Orange is imcompatible with newer CommonMark,
 # install the module manually so that it does not get installed.
 # Remove this when the minimum supported Orange is 3.16.
 pip install CommonMark==0.7.5
 
 if [ $ORANGE == "release" ]; then
     echo "Orange: Skipping separate Orange install"
-    return 0
-fi
-
-if [ $ORANGE == "master" ]; then
+elif [ $ORANGE == "master" ]; then
     echo "Orange: from git master"
     pip install https://github.com/biolab/orange3/archive/master.zip
-    return $?;
+else
+    PACKAGE="orange3==$ORANGE"
+    echo "Orange: installing version $PACKAGE"
+    pip install $PACKAGE
 fi
 
-PACKAGE="orange3==$ORANGE"
-echo "Orange: installing version $PACKAGE"
-pip install $PACKAGE
+# some widgets works with network add-on
+if [ -n "$ORANGE3_NETWORK" ] && [ "$ORANGE3_NETWORK" = true ] ; then
+  pip install orange3-network
+fi
