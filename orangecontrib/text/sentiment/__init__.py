6f8422afd246c3c99d447e8b8bd53edead1f45f3
Ajda Pretnar <ajdapretnar@gmail.com>
2020-12-14 17:36:00 +0100

    Refactor Sentiment Analysis

--- orangecontrib/text/sentiment/__init__.py
+++ orangecontrib/text/sentiment/__init__.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import re
 
 import serverfiles
@@ -12,11 +13,8 @@ from Orange.misc.environ import data_dir
 from orangecontrib.text.misc import wait_nltk_data
 
 from orangecontrib.text import Corpus
-from orangecontrib.text.preprocess import WordPunctTokenizer
 from orangecontrib.text.vectorization.base import SharedTransform, \
     VectorizationComputeValue
-from orangecontrib.text.sentiment.filter_lexicon import FilterSentiment, \
-    SloSentiment
 
 
 def read_file(file):
@@ -24,9 +22,53 @@ def read_file(file):
         return f.read().split('\n')
 
 
-class LiuHuSentiment:
-    sentiments = ('sentiment',)
+def compute_from_dict(tokens, pos, neg):
+    scores = []
+    for doc in tokens:
+        pos_words = len(pos.intersection(doc)) if pos else 0
+        neg_words = len(neg.intersection(doc)) if neg else 0
+        scores.append([100 * (pos_words - neg_words) / max(len(doc), 1)])
+    return scores
+
+
+class Sentiment:
+    sentiments = None
+    name = None
+
+    def get_scores(self, corpus):
+        return NotImplemented
+
+    def transform(self, corpus):
+        scores = self.get_scores(corpus)
+        X = np.array(scores).reshape((-1, len(self.sentiments)))
+
+        # set compute values
+        shared_cv = SharedTransform(self, corpus.used_preprocessor)
+        cv = [VectorizationComputeValue(shared_cv, col)
+              for col in self.sentiments]
+
+        corpus = corpus.extend_attributes(X, self.sentiments, compute_values=cv)
+        return corpus
+
+
+class LiuHuSentiment(Sentiment):
+    sentiments = ('sentiment',)  # output column names
     name = 'Liu Hu'
+
+    class SloSentiment:
+        resources_folder = os.path.dirname(__file__)
+
+        @classmethod
+        def positive(cls):
+            f = os.path.join(cls.resources_folder,
+                             'resources/positive_words_Slolex.txt')
+            return read_file(f)
+
+        @classmethod
+        def negative(cls):
+            f = os.path.join(cls.resources_folder,
+                             'resources/negative_words_Slolex.txt')
+            return read_file(f)
 
     methods = {'English': opinion_lexicon,
                'Slovenian': SloSentiment}
@@ -37,53 +79,91 @@ class LiuHuSentiment:
         self.positive = set(self.methods[self.language].positive())
         self.negative = set(self.methods[self.language].negative())
 
-    def transform(self, corpus):
-        scores = []
-        corpus = WordPunctTokenizer()(corpus)
-
-        for doc in corpus.tokens:
-            pos_words = sum(word in self.positive for word in doc)
-            neg_words = sum(word in self.negative for word in doc)
-            scores.append([100*(pos_words - neg_words)/max(len(doc), 1)])
-        X = np.array(scores).reshape((-1, len(self.sentiments)))
-
-        # set  compute values
-        shared_cv = SharedTransform(self)
-        cv = [VectorizationComputeValue(shared_cv, col)
-              for col in self.sentiments]
-
-        corpus = corpus.extend_attributes(X, self.sentiments, compute_values=cv)
-        return corpus
+    def get_scores(self, corpus):
+        return compute_from_dict(corpus.tokens, self.positive, self.negative)
 
 
-class VaderSentiment:
-    sentiments = ('pos', 'neg', 'neu', 'compound')
+class VaderSentiment(Sentiment):
+    sentiments = ('pos', 'neg', 'neu', 'compound') # output column names
     name = 'Vader'
 
     @wait_nltk_data
     def __init__(self):
         self.vader = SentimentIntensityAnalyzer()
 
-    def transform(self, corpus):
+    def get_scores(self, corpus):
         scores = []
         for text in corpus.documents:
             pol_sc = self.vader.polarity_scores(text)
             scores.append([pol_sc[x] for x in self.sentiments])
-        X = np.array(scores).reshape((-1, len(self.sentiments)))
-
-        # set  compute values
-        shared_cv = SharedTransform(self)
-        cv = [VectorizationComputeValue(shared_cv, col)
-              for col in self.sentiments]
-
-        corpus = corpus.extend_attributes(X, self.sentiments, compute_values=cv)
-        return corpus
+        return scores
 
 
-class MultiSentiment:
+class SentimentDictionaries:
+    server_url = None
+
+    def __init__(self):
+        self.local_path = os.path.join(data_dir(versioned=False), 'sentiment/')
+        self.serverfiles = serverfiles.ServerFiles(self.server_url)
+        self.localfiles = serverfiles.LocalFiles(self.local_path,
+                                                 serverfiles=self.serverfiles)
+        self._supported_languages = []
+
+    def __getitem__(self, language):
+        return NotImplemented
+
+    @property
+    def supported_languages(self):
+        return NotImplemented
+
+    @property
+    def lang_files(self):
+        try:
+            return self.serverfiles.listfiles()
+        except ConnectionError:
+            return self.localfiles.listfiles()
+
+    @property
+    def online(self):
+        try:
+            self.serverfiles.listfiles()
+            return True
+        except ConnectionError:
+            return False
+
+
+class MultisentimentDictionaries(SentimentDictionaries):
+    server_url = "http://file.biolab.si/files/sentiment/"
+
+    def __init__(self):
+        super().__init__()
+
+    def __getitem__(self, language):
+        pos = set(read_file(self.localfiles.localpath_download(
+            f"positive_words_{language}.txt")))
+        neg = set(read_file(self.localfiles.localpath_download(
+            f"negative_words_{language}.txt")))
+        return pos, neg
+
+    def supported_languages(self):
+        re_pos = "positive_words_(.*)\.txt"
+        re_neg = "negative_words_(.*)\.txt"
+        pos = neg = set()
+        for i in self.lang_files:
+            res_pos = re.fullmatch(re_pos, i[0])
+            res_neg = re.fullmatch(re_neg, i[0])
+            if res_pos:
+                pos.add(res_pos.group(1))
+            elif res_neg:
+                neg.add(res_neg.group(1))
+        return pos.intersection(neg)
+
+
+class MultiSentiment(Sentiment):
     sentiments = ('sentiment',)
     name = 'Multilingual Sentiment'
 
+    # mapping for nicer language labels
     LANGS = {'Afrikaans': 'af', 'Arabic': 'ar', 'Azerbaijani': 'az',
              'Belarusian': 'be', 'Bulgarian': 'bg',
              'Bosnian': 'bs', 'Catalan': 'ca', 'Czech': 'cs',
@@ -101,44 +181,12 @@ class MultiSentiment:
              'Chinese Characters': 'zhw'}
 
     def __init__(self, language='English'):
-        self._language = language
-        self.dictionaries = SentimentDictionaries()
-        self.positive = None
-        self.negative = None
+        self.language = language
+        code = self.LANGS[self.language]
+        self.positive, self.negative = MultisentimentDictionaries()[code]
 
-    def load_dict(self):
-        if self.positive is None or self.negative is None:
-            code = self.LANGS[self._language]
-            self.positive, self.negative = self.dictionaries[code]
-
-    def transform(self, corpus):
-        self.load_dict()
-        scores = []
-        corpus = WordPunctTokenizer()(corpus)
-
-        for doc in corpus.tokens:
-            pos_words = sum(word in self.positive for word in doc)
-            neg_words = sum(word in self.negative for word in doc)
-            scores.append([100 * (pos_words - neg_words) / max(len(doc), 1)])
-        X = np.array(scores).reshape((-1, len(self.sentiments)))
-
-        # set  compute values
-        shared_cv = SharedTransform(self)
-        cv = [VectorizationComputeValue(shared_cv, col)
-              for col in self.sentiments]
-
-        corpus = corpus.extend_attributes(X, self.sentiments, compute_values=cv)
-        return corpus
-
-    @property
-    def language(self):
-        return self._language
-
-    @language.setter
-    def language(self, value):
-        self._language = value
-        self.positive = None
-        self.negative = None
+    def get_scores(self, corpus):
+        return compute_from_dict(corpus.tokens, self.positive, self.negative)
 
     def __getstate__(self):
         return {'language': self.language}
@@ -147,48 +195,8 @@ class MultiSentiment:
         self.__init__(state['language'])
 
 
-class SentimentDictionaries:
-    server_url = "http://file.biolab.si/files/sentiment/"
-
-    def __init__(self):
-        self.local_data = os.path.join(data_dir(versioned=False), 'sentiment/')
-        self.serverfiles = serverfiles.ServerFiles(self.server_url)
-        self.localfiles = serverfiles.LocalFiles(self.local_data,
-                                                 serverfiles=self.serverfiles)
-        self._supported_languages = []
-
-    def __getitem__(self, language):
-        pos = set(FilterSentiment.read_file(self.localfiles.localpath_download(
-            f"positive_words_{language}.txt")))
-        neg = set(FilterSentiment.read_file(self.localfiles.localpath_download(
-            f"negative_words_{language}.txt")))
-        return pos, neg
-
-    @property
-    def lang_files(self):
-        try:
-            return self.serverfiles.listfiles()
-        except ConnectionError:
-            return self.localfiles.listfiles()
-
-    @property
-    def supported_languages(self):
-        regex = "(?<=negative_words_|positive_words_)(.*)(?=\.txt)"
-        self._supported_languages = [re.search(regex, i[0]).group(0) for i in
-                                     list(self.lang_files)]
-        return self._supported_languages
-
-    @property
-    def online(self):
-        try:
-            self.serverfiles.listfiles()
-            return True
-        except ConnectionError:
-            return False
-
-
-class CustomDictionaries:
-    sentiments = ['sentiment']
+class CustomDictionaries(Sentiment):
+    sentiments = ('sentiment',)
     name = 'Custom Dictionaries'
 
     @wait_nltk_data
@@ -196,25 +204,8 @@ class CustomDictionaries:
         self.positive = set(read_file(pos)) if pos else None
         self.negative = set(read_file(neg)) if neg else None
 
-    def transform(self, corpus):
-        scores = []
-        corpus = WordPunctTokenizer()(corpus)
-
-        for doc in corpus.tokens:
-            pos_words = sum(word in self.positive for word in doc) if \
-                self.positive else 0
-            neg_words = sum(word in self.negative for word in doc) if \
-                self.negative else 0
-            scores.append([100*(pos_words - neg_words)/max(len(doc), 1)])
-        X = np.array(scores).reshape((-1, len(self.sentiments)))
-
-        # set  compute values
-        shared_cv = SharedTransform(self)
-        cv = [VectorizationComputeValue(shared_cv, col)
-              for col in self.sentiments]
-
-        corpus = corpus.extend_attributes(X, self.sentiments, compute_values=cv)
-        return corpus
+    def get_scores(self, corpus):
+        return compute_from_dict(corpus.tokens, self.positive, self.negative)
 
 
 if __name__ == "__main__":
