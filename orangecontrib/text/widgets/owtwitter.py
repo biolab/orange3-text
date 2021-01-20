11f37d08d6f0ece38a83ecc711cadd42afa9444e
Primož Godec <p.godec9@gmail.com>
2021-01-20 13:48:52 +0100

    Fix twitter: use concurrent mixin + add tests

--- orangecontrib/text/widgets/owtwitter.py
+++ orangecontrib/text/widgets/owtwitter.py
@@ -1,31 +1,75 @@
-from AnyQt.QtCore import Qt
-from AnyQt.QtWidgets import QApplication, QGridLayout, QLabel, QFormLayout
+from typing import List, Optional
 
+from AnyQt.QtCore import Qt
+from AnyQt.QtWidgets import QGridLayout, QLabel, QFormLayout
+
+from orangewidget.utils.widgetpreview import WidgetPreview
 from Orange.widgets import gui
 from Orange.widgets.credentials import CredentialManager
 from Orange.widgets.settings import Setting
 from Orange.widgets.widget import OWWidget, Msg, Output
+from Orange.widgets.utils.concurrent import TaskState, ConcurrentWidgetMixin
+from tweepy import TweepError
+
 from orangecontrib.text import twitter
 from orangecontrib.text.corpus import Corpus
 from orangecontrib.text.language_codes import lang2code
-from orangecontrib.text.widgets.utils import (ComboBox, ListEdit,
-                                              CheckListLayout, gui_require, asynchronous)
+from orangecontrib.text.twitter import TwitterAPI
+from orangecontrib.text.widgets.utils import (
+    ComboBox,
+    ListEdit,
+    CheckListLayout,
+    gui_require,
+)
 
 
-class OWTwitter(OWWidget):
+def search(
+    api: TwitterAPI,
+    max_tweets: int,
+    word_list: List[str],
+    collecting: bool,
+    language: Optional[str],
+    allow_retweets: Optional[bool],
+    mode: str,
+    state: TaskState,
+):
+    def advance(progress):
+        if state.is_interruption_requested():
+            raise Exception
+        state.set_progress_value(progress * 100)
+
+    if mode == "content":
+        return api.search_content(
+            max_tweets=max_tweets,
+            content=word_list,
+            lang=language,
+            allow_retweets=allow_retweets,
+            collecting=collecting,
+            callback=advance,
+        )
+    elif mode == "authors":
+        return api.search_authors(
+            max_tweets=max_tweets,
+            authors=word_list,
+            collecting=collecting,
+            callback=advance,
+        )
+
+
+class OWTwitter(OWWidget, ConcurrentWidgetMixin):
     class APICredentialsDialog(OWWidget):
-        name = 'Twitter API Credentials'
+        name = "Twitter API Credentials"
         want_main_area = False
         resizing_enabled = False
 
-        cm_key = CredentialManager('Twitter API Key')
-        cm_secret = CredentialManager('Twitter API Secret')
+        cm_key = CredentialManager("Twitter API Key")
+        cm_secret = CredentialManager("Twitter API Secret")
 
-        key_input = ''
-        secret_input = ''
+        key_input = ""
+        secret_input = ""
 
         class Error(OWWidget.Error):
-            invalid_credentials = Msg('This credentials are invalid.')
+            invalid_credentials = Msg("This credentials are invalid.")
 
         def __init__(self, parent):
             super().__init__()
@@ -34,16 +78,19 @@ class OWTwitter(OWWidget):
 
             form = QFormLayout()
             form.setContentsMargins(5, 5, 5, 5)
-            self.key_edit = gui.lineEdit(self, self, 'key_input',
-                                         controlWidth=400)
-            form.addRow('Key:', self.key_edit)
-            self.secret_edit = gui.lineEdit(self, self, 'secret_input',
-                                            controlWidth=400)
-            form.addRow('Secret:', self.secret_edit)
+            self.key_edit = gui.lineEdit(
+                self, self, "key_input", controlWidth=400
+            )
+            form.addRow("Key:", self.key_edit)
+            self.secret_edit = gui.lineEdit(
+                self, self, "secret_input", controlWidth=400
+            )
+            form.addRow("Secret:", self.secret_edit)
             self.controlArea.layout().addLayout(form)
 
-            self.submit_button = gui.button(self.controlArea, self, 'OK',
-                                            self.accept)
+            self.submit_button = gui.button(
+                self.controlArea, self, "OK", self.accept
+            )
             self.load_credentials()
 
         def load_credentials(self):
@@ -64,7 +111,8 @@ class OWTwitter(OWWidget):
                 self.credentials = c
 
         def accept(self, silent=False):
-            if not silent: self.Error.invalid_credentials.clear()
+            if not silent:
+                self.Error.invalid_credentials.clear()
             self.check_credentials()
             if self.credentials and self.credentials.valid:
                 self.parent.update_api(self.credentials)
@@ -72,9 +120,9 @@ class OWTwitter(OWWidget):
             elif not silent:
                 self.Error.invalid_credentials()
 
-    name = 'Twitter'
-    description = 'Load tweets from the Twitter API.'
-    icon = 'icons/Twitter.svg'
+    name = "Twitter"
+    description = "Load tweets from the Twitter API."
+    icon = "icons/Twitter.svg"
     priority = 150
 
     class Outputs:
@@ -84,19 +132,17 @@ class OWTwitter(OWWidget):
     resizing_enabled = False
 
     class Warning(OWWidget.Warning):
-        no_text_fields = Msg('Text features are inferred when none selected.')
+        no_text_fields = Msg("Text features are inferred when none selected.")
 
     class Error(OWWidget.Error):
-        api = Msg('Api error ({})')
-        rate_limit = Msg('Rate limit exceeded. Please try again later.')
-        empty_authors = Msg('Please provide some authors.')
-        wrong_authors = Msg('Query does not match Twitter user handle.')
-        key_missing = Msg('Please provide a valid API key to get the data.')
-
-    tweets_info = 'Tweets on output: {}'
+        api_error = Msg("Api error ({})")
+        rate_limit = Msg("Rate limit exceeded. Please try again later.")
+        empty_authors = Msg("Please provide some authors.")
+        wrong_authors = Msg("Query does not match Twitter user handle.")
+        key_missing = Msg("Please provide a valid API key to get the data.")
 
     CONTENT, AUTHOR = 0, 1
-    MODES = ['Content', 'Author']
+    MODES = ["Content", "Author"]
     word_list = Setting([])
     mode = Setting(0)
     limited_search = Setting(True)
@@ -108,26 +154,30 @@ class OWTwitter(OWWidget):
     attributes = [f.name for f in twitter.TwitterAPI.string_attributes]
     text_includes = Setting([f.name for f in twitter.TwitterAPI.text_features])
 
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        OWWidget.__init__(self)
+        ConcurrentWidgetMixin.__init__(self)
         self.api = None
         self.corpus = None
         self.api_dlg = self.APICredentialsDialog(self)
         self.api_dlg.accept(silent=True)
 
         # Set API key button
-        gui.button(self.controlArea, self, 'Twitter API Key',
-                   callback=self.open_key_dialog,
-                   tooltip='Set the API key for this widget.',
-                   focusPolicy=Qt.NoFocus)
+        gui.button(
+            self.controlArea,
+            self,
+            "Twitter API Key",
+            callback=self.open_key_dialog,
+            tooltip="Set the API key for this widget.",
+            focusPolicy=Qt.NoFocus,
+        )
 
         # Query
-        query_box = gui.hBox(self.controlArea, 'Query')
+        query_box = gui.hBox(self.controlArea, "Query")
         layout = QGridLayout()
         layout.setVerticalSpacing(5)
-        layout.setColumnStretch(2, 1)           # stretch last columns
-        layout.setColumnMinimumWidth(1, 15)     # add some space for checkbox
+        layout.setColumnStretch(2, 1)  # stretch last columns
+        layout.setColumnMinimumWidth(1, 15)  # add some space for checkbox
         ROW = 0
         COLUMNS = 3
 
@@ -136,57 +186,83 @@ class OWTwitter(OWWidget):
             layout.addWidget(QLabel(label), ROW, 0)
             if isinstance(items, tuple):
                 for i, item in enumerate(items):
-                    layout.addWidget(item, ROW, 1+i)
+                    layout.addWidget(item, ROW, 1 + i)
             else:
-                layout.addWidget(items, ROW, 1, 1, COLUMNS-1)
+                layout.addWidget(items, ROW, 1, 1, COLUMNS - 1)
             ROW += 1
 
         # Query input
-        add_row('Query word list:',
-                ListEdit(self, 'word_list',
-                         'Multiple lines are joined with OR.', 80, self))
+        add_row(
+            "Query word list:",
+            ListEdit(
+                self,
+                "word_list",
+                "Multiple lines are joined with OR.",
+                80,
+                self,
+            ),
+        )
 
         # Search mode
-        add_row('Search by:',
-                gui.comboBox(self, self, 'mode', items=self.MODES,
-                             callback=self.mode_toggle))
+        add_row(
+            "Search by:",
+            gui.comboBox(
+                self, self, "mode", items=self.MODES, callback=self.mode_toggle
+            ),
+        )
 
         # Language
-        self.language_combo = ComboBox(self, 'language',
-                                       items=(('Any', None),) +
-                                             tuple(sorted(lang2code.items())))
-        add_row('Language:', self.language_combo)
+        self.language_combo = ComboBox(
+            self,
+            "language",
+            items=(("Any", None),) + tuple(sorted(lang2code.items())),
+        )
+        add_row("Language:", self.language_combo)
 
         # Max tweets
-        add_row('Max tweets:',
-                gui.spin(self, self, 'max_tweets', minv=1, maxv=10000,
-                         checked='limited_search'))
+        add_row(
+            "Max tweets:",
+            gui.spin(
+                self,
+                self,
+                "max_tweets",
+                minv=1,
+                maxv=10000,
+                checked="limited_search",
+            ),
+        )
 
         # Retweets
-        self.retweets_checkbox = gui.checkBox(self, self, 'allow_retweets', '', minimumHeight=30)
-        add_row('Allow retweets:', self.retweets_checkbox)
+        self.retweets_checkbox = gui.checkBox(
+            self, self, "allow_retweets", "", minimumHeight=30
+        )
+        add_row("Allow retweets:", self.retweets_checkbox)
 
         # Collect Results
-        add_row('Collect results:',
-                gui.checkBox(self, self, 'collecting', ''))
+        add_row("Collect results:", gui.checkBox(self, self, "collecting", ""))
 
         query_box.layout().addLayout(layout)
 
         self.controlArea.layout().addWidget(
-            CheckListLayout('Text includes', self, 'text_includes',
-                            self.attributes, cols=2,
-                            callback=self.set_text_features))
-
-        self.tweets_info_label = gui.label(self.controlArea, self,
-                                           self.tweets_info.format(0),
-                                           box='Info')
+            CheckListLayout(
+                "Text includes",
+                self,
+                "text_includes",
+                self.attributes,
+                cols=2,
+                callback=self.set_text_features,
+            )
+        )
 
         # Buttons
         self.button_box = gui.hBox(self.controlArea)
-
-        self.search_button = gui.button(self.button_box, self, 'Search',
-                                        self.start_stop,
-                                        focusPolicy=Qt.NoFocus)
+        self.search_button = gui.button(
+            self.button_box,
+            self,
+            "Search",
+            self.start_stop,
+            focusPolicy=Qt.NoFocus,
+        )
 
         self.mode_toggle()
         self.setFocus()  # to widget itself to show placeholder for query_edit
@@ -202,70 +278,77 @@ class OWTwitter(OWWidget):
         self.language_combo.setEnabled(self.mode == self.CONTENT)
 
     def start_stop(self):
-        if self.search.running:
-            self.search.stop()
+        if self.task:
+            self.cancel()
+            self.search_button.setText("Search")
         else:
             self.run_search()
 
-    @gui_require('api', 'key_missing')
+    @gui_require("api", "key_missing")
     def run_search(self):
         self.Error.clear()
         self.search()
 
-    @asynchronous
     def search(self):
         max_tweets = self.max_tweets if self.limited_search else 0
 
         if self.mode == self.CONTENT:
-            return self.api.search_content(max_tweets=max_tweets,
-                                           content=self.word_list,
-                                           lang=self.language,
-                                           allow_retweets=self.allow_retweets,
-                                           collecting=self.collecting)
+            self.start(
+                search,
+                self.api,
+                max_tweets,
+                self.word_list,
+                self.collecting,
+                self.language,
+                self.allow_retweets,
+                "content",
+            )
         else:
             if not self.word_list:
                 self.Error.empty_authors()
                 return None
-            if not any(a.startswith('@') for a in self.word_list):
+            if not any(a.startswith("@") for a in self.word_list):
                 self.Error.wrong_authors()
                 return None
-            return self.api.search_authors(max_tweets=max_tweets,
-                                           authors=self.word_list,
-                                           collecting=self.collecting)
+            self.start(
+                search,
+                self.api,
+                max_tweets,
+                self.word_list,
+                self.collecting,
+                None,
+                None,
+                "authors",
+            )
+        self.search_button.setText("Stop")
 
     def update_api(self, key):
         if key:
             self.Error.key_missing.clear()
-            self.api = twitter.TwitterAPI(key,
-                                          on_error=self.Error.api,
-                                          on_rate_limit=self.Error.rate_limit,
-                                          should_break=self.search.should_break,
-                                          on_progress=self.update_tweets_num)
+            self.api = twitter.TwitterAPI(key)
         else:
             self.api = None
 
-    @search.on_start
-    def on_start(self):
-        self.progressBarInit()
-        self.search_button.setText('Stop')
-        self.Outputs.corpus.send(None)
-        if self.mode == self.CONTENT and not self.limited_search:
-            self.progressBarFinished()
-
-    @search.on_result
-    def on_result(self, result):
-        self.search_button.setText('Search')
-        self.tweets_info_label.setText(self.tweets_info.format(len(result) if result else 0))
+    def on_done(self, result):
+        self.search_button.setText("Search")
+        if result:
+            self.info.set_output_summary(
+                len(result), f"{len(result)} tweets on output"
+            )
+        else:
+            self.info.set_output_summary(self.info.NoOutput)
         self.corpus = result
         self.set_text_features()
-        self.progressBarFinished()
 
-    @search.callback(should_raise=False)
-    def update_tweets_num(self, num=0, progress=None):
-        if self.limited_search or self.mode == self.AUTHOR:
-            if progress is not None:
-                self.progressBarSet(100 * progress)
-        self.tweets_info_label.setText(self.tweets_info.format(num))
+    def on_exception(self, ex):
+        self.search_button.setText("Search")
+        if isinstance(ex, TweepError) and ex.response.status_code == 429:
+            self.Error.rate_limit()
+        else:
+            self.Error.api_error(str(ex))
+
+    def on_partial_result(self, _):
+        pass
 
     def set_text_features(self):
         self.Warning.no_text_fields.clear()
@@ -273,20 +356,19 @@ class OWTwitter(OWWidget):
             self.Warning.no_text_fields()
 
         if self.corpus is not None:
-            vars_ = [var for var in self.corpus.domain.metas
-                     if var.name in self.text_includes]
+            vars_ = [
+                var
+                for var in self.corpus.domain.metas
+                if var.name in self.text_includes
+            ]
             self.corpus.set_text_features(vars_ or None)
             self.Outputs.corpus.send(self.corpus)
 
-    @gui_require('api', 'key_missing')
+    @gui_require("api", "key_missing")
     def send_report(self):
         for task in self.api.search_history:
             self.report_items(task)
 
 
-if __name__ == '__main__':
-    app = QApplication([])
-    widget = OWTwitter()
-    widget.show()
-    app.exec()
-    widget.saveSettings()
+if __name__ == "__main__":
+    WidgetPreview(OWTwitter).run()
--- orangecontrib/text/widgets/tests/test_owtwitter.py
+++ orangecontrib/text/widgets/tests/test_owtwitter.py
@@ -1,10 +1,32 @@
+import unittest
+from unittest.mock import patch
+
+import numpy as np
+from Orange.data import Domain, StringVariable
 from Orange.widgets.tests.base import WidgetTest
+from orangecontrib.text import twitter, Corpus
+from orangecontrib.text.tests.test_twitter import Response
 from orangecontrib.text.widgets.owtwitter import OWTwitter
+from tweepy import TweepError
+
+
+# it is not possible to test real API because API key cannot be shared for
+# tests so we use dummy_fetch instead
+def dummy_fetch(self, cursors, max_tweets, search_author, callback):
+    return (
+        Corpus(
+            Domain([], metas=[StringVariable("Content")]),
+            metas=np.array([["Abc"], ["Cde"], ["Gf"]]),
+        ),
+        3,
+    )
 
 
 class TestTwitterWidget(WidgetTest):
     def setUp(self):
         self.widget = self.create_widget(OWTwitter)
+        # give some key to api - to allow start the search
+        self.widget.update_api(twitter.Credentials("testkey", "testsecret"))
 
     def test_no_error(self):
         self.widget.search()
@@ -13,6 +35,59 @@ class TestTwitterWidget(WidgetTest):
     def test_empty_author_list(self):
         self.widget.mode = 1
         self.widget.mode_toggle()
-        self.widget.search()
+        self.widget.search_button.click()
         self.wait_until_finished()
         self.assertTrue(self.widget.Error.empty_authors.is_shown())
+        self.assertIsNone(self.get_output(self.widget.Outputs.corpus))
+
+    @patch("orangecontrib.text.twitter.TwitterAPI.fetch", dummy_fetch)
+    def test_content_search(self):
+        self.widget.word_list = ["orange"]
+        self.widget.search_button.click()
+        output = self.get_output(self.widget.Outputs.corpus)
+        self.assertEqual(3, len(output))
+        self.assertGreater(len(str(output[0, "Content"])), 0)
+
+    @patch("orangecontrib.text.twitter.TwitterAPI.fetch", dummy_fetch)
+    def test_author(self):
+        self.widget.mode = 1
+        self.widget.word_list = ["@OrangeDataMiner"]
+        self.widget.mode_toggle()
+        self.widget.search_button.click()
+
+        output = self.get_output(self.widget.Outputs.corpus)
+        self.assertEqual(3, len(output))
+        self.assertGreater(len(str(output[0, "Content"])), 0)
+
+    @patch("orangecontrib.text.twitter.TwitterAPI.fetch", dummy_fetch)
+    def test_summary(self):
+        info = self.widget.info
+        self.widget.word_list = ["orange"]
+        self.widget.search_button.click()
+        self.wait_until_finished()
+        self.assertEqual("3", info._StateInfo__output_summary.brief)
+        self.assertEqual(
+            "3 tweets on output", info._StateInfo__output_summary.details
+        )
+
+    @patch("tweepy.Cursor.items")
+    def test_rate_limit(self, mock_items):
+        mock_items.side_effect = TweepError("Rate limit error", Response(429))
+        self.widget.word_list = ["orange"]
+        self.widget.search_button.click()
+        self.wait_until_finished()
+        self.assertTrue(self.widget.Error.rate_limit.is_shown())
+        self.assertEqual("Search", self.widget.search_button.text())
+
+    @patch("tweepy.Cursor.items")
+    def test_error(self, mock_items):
+        mock_items.side_effect = TweepError("Other errors", Response(400))
+        self.widget.word_list = ["orange"]
+        self.widget.search_button.click()
+        self.wait_until_finished()
+        self.assertTrue(self.widget.Error.api_error.is_shown())
+        self.assertEqual("Search", self.widget.search_button.text())
+
+
+if __name__ == "__main__":
+    unittest.main()
