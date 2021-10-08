24c9fc0006f98307138de702ab4212c612373c2e
Ajda Pretnar <ajdapretnar@gmail.com>
2021-10-08 14:53:11 +0200

    Release 1.5.2

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ except ImportError:
 NAME = 'Orange3-Text'
 
 MAJOR = 1
-MINOR = 6
-MICRO = 0
-IS_RELEASED = False
+MINOR = 5
+MICRO = 2
+IS_RELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 FULL_VERSION = VERSION
 
