0f5c7b0608c06e0bc855549297356399b04b97ae
Ajda <ajdapretnar@users.noreply.github.com>
2021-10-13 09:54:36 +0200

    Update orangecontrib/text/topics/topics.py
    Co-authored-by: Primož Godec <p.godec9@gmail.com>

--- orangecontrib/text/topics/topics.py
+++ orangecontrib/text/topics/topics.py
@@ -26,10 +26,6 @@ class Topics(Table):
     """ Dummy wrapper for Table so signals can distinguish All Topics from Data.
     """
 
-    def __new__(cls, *args, **kwargs):
-        """ Bypass Table.__new__. """
-        return object.__new__(Topics)
-
 
 class GensimWrapper:
     name = NotImplemented
