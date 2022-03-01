656d1c55ecfee94f0fca5abb58f5316084368b03
Vesna Tanko <tankovesna@hotmail.com>
2022-03-01 09:14:08 +0100

    Ontology: Minor changes and tests

--- orangecontrib/text/widgets/owontology.py
+++ orangecontrib/text/widgets/owontology.py
@@ -1,28 +1,26 @@
-import time
-from typing import Optional, List, Tuple, Any
+from typing import Optional, List, Tuple, Any, Dict
+
 import numpy as np
+from AnyQt.QtCore import Qt
+from AnyQt.QtWidgets import QTreeWidget, QTreeWidgetItem
 from networkx import Graph, minimum_spanning_tree
+from networkx.algorithms.centrality import voterank
 from networkx.convert_matrix import from_numpy_array
 from networkx.relabel import relabel_nodes
-from networkx.algorithms.centrality import voterank
 from sentence_transformers import SentenceTransformer
 
-from AnyQt.QtWidgets import QTreeWidget, QTreeWidgetItem
-from AnyQt.QtCore import Qt
-
 from Orange.data import Table, StringVariable, Domain
-from Orange.widgets.utils.concurrent import ConcurrentWidgetMixin
-from Orange.widgets.widget import OWWidget
 from Orange.widgets import gui
-from orangewidget.utils.signals import Input
-from orangewidget.widget import Msg
+from Orange.widgets.utils.concurrent import ConcurrentWidgetMixin, TaskState
+from Orange.widgets.widget import OWWidget, Msg, Input
+
+WORDS_COLUMN_NAME = "Words"
 
 
 def _generate_ontology(words: List[str]) -> Tuple[Graph, str]:
     """
     Ontology generator -- computes MST on the complete graph of words.
     """
-    t = time.time()
     model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
     embs = np.array(model.encode(words))
     norms = np.linalg.norm(embs, axis=1).reshape(-1, 1)
@@ -36,7 +34,7 @@ def _generate_ontology(words: List[str]) -> Tuple[Graph, str]:
     return mst, root
 
 
-def to_tree(graph: Graph, root: str, prev: str = None):
+def to_tree(graph: Graph, root: str, prev: str = None) -> Dict:
     neighbors = [n for n in graph.neighbors(root) if n != prev]
     tree = {}
     for neighbor in neighbors:
@@ -44,13 +42,22 @@ def to_tree(graph: Graph, root: str, prev: str = None):
     return tree
 
 
-def _run(words: List[str], _):
+def _run(words: List[str], state: TaskState) -> Dict:
+    def callback(i: float, status=""):
+        state.set_progress_value(i * 100)
+        if status:
+            state.set_status(status)
+        if state.is_interruption_requested():
+            raise Exception
+
+    callback(0, "Calculating...")
+
     if len(words) > 1:
         words = [w.lower() for w in words]
         mst, root = _generate_ontology(words)
         return {root: to_tree(mst, root)}
     elif len(words) == 1:
-        return {words[0]: {}}
+        return {words[0].lower(): {}}
     else:
         return {}
 
@@ -66,14 +73,12 @@ class OWOntology(OWWidget, ConcurrentWidgetMixin):
         words = Input("Words", Table)
 
     class Warning(OWWidget.Warning):
-        no_string_vars = Msg("Input needs at least one Text variable.")
+        no_words_column = Msg("Input is missing 'Words' column.")
 
     def __init__(self):
         OWWidget.__init__(self)
         ConcurrentWidgetMixin.__init__(self)
-        self.__words = None
-
-        box = gui.vBox(self.mainArea, "Projects")
+        box = gui.vBox(self.mainArea)
 
         self.tree = QTreeWidget()
         self.tree.setColumnCount(1)
@@ -83,24 +88,14 @@ class OWOntology(OWWidget, ConcurrentWidgetMixin):
 
     @Inputs.words
     def set_words(self, words: Optional[Table]):
-        self._check_input_words(words)
-        self.start_plot()
-
-    def _check_input_words(self, words_table):
-        self.Warning.no_string_vars.clear()
-        if words_table:
-            metas = words_table.domain.metas
-            word_vars = (m for m in metas if isinstance(m, StringVariable))
-            if not word_vars:
-                self.Warning.no_string_vars()
-                self.__words = None
+        self.Warning.no_words_column.clear()
+        if words:
+            if WORDS_COLUMN_NAME in words.domain and words.domain[
+                    WORDS_COLUMN_NAME].attributes.get("type") == "words":
+                words = list(words.get_column_view(WORDS_COLUMN_NAME)[0])
+                self.start(_run, words)
             else:
-                words_var = next(word_vars)
-                self.__words = words_table.get_column_view(words_var)[0]
-
-    def start_plot(self):
-        if self.__words is not None:
-            self.start(_run, self.__words)
+                self.Warning.no_words_column()
 
     def on_done(self, tree):
         self.tree.clear()
@@ -129,8 +124,7 @@ class OWOntology(OWWidget, ConcurrentWidgetMixin):
 if __name__ == "__main__":
     from Orange.widgets.utils.widgetpreview import WidgetPreview
 
-    words_vars = [StringVariable("S1")]
-    words = [
+    lst = [
         "Stvar",
         "Agent",
         "Organ",
@@ -159,6 +153,8 @@ if __name__ == "__main__":
         "Odločba",
         "Sklep",
     ]
-    input_table = Table.from_list(Domain([], metas=words_vars), [[w] for w in words])
-
-    WidgetPreview(OWOntology).run(input_table)
+    words_var = StringVariable("Words")
+    words_var.attributes = {"type": "words"}
+    words_ = Table.from_list(Domain([], metas=[words_var]), [[w] for w in lst])
+    words_.name = "Words"
+    WidgetPreview(OWOntology).run(words_)
---
+++ orangecontrib/text/widgets/tests/test_owontology.py
@@ -0,0 +1,63 @@
+# pylint: disable=missing-docstring
+import unittest
+from typing import List
+from unittest.mock import Mock
+
+from Orange.data import StringVariable, Table, Domain
+from Orange.widgets.tests.base import WidgetTest
+from orangecontrib.text.widgets.owontology import OWOntology, _run
+
+
+def create_words_table(words: List) -> Table:
+    words_var = StringVariable("Words")
+    words_var.attributes = {"type": "words"}
+    domain = Domain([], metas=[words_var])
+    data = [[w] for w in words]
+    words = Table.from_list(domain, data)
+    words.name = "Words"
+    return words
+
+
+class TestRunner(unittest.TestCase):
+    def setUp(self):
+        self.words = ["foo", "bar"]
+        self.state = Mock()
+        self.state.is_interruption_requested = Mock(return_value=False)
+
+    def test_run(self):
+        result = _run(self.words, self.state)
+        self.assertEqual(result, {'foo': {'bar': {}}})
+
+    def test_run_single_word(self):
+        result = _run(["foo"], self.state)
+        self.assertEqual(result, {"foo": {}})
+
+    def test_run_empty(self):
+        result = _run([], self.state)
+        self.assertEqual(result, {})
+
+    def test_run_interrupt(self):
+        state = Mock()
+        state.is_interruption_requested = Mock(return_value=True)
+        self.assertRaises(Exception, _run, self.words, state)
+
+
+class TestOWKeywords(WidgetTest):
+    def setUp(self):
+        self.widget = self.create_widget(OWOntology)
+
+    def test_input_words(self):
+        words = create_words_table(["foo"])
+        self.send_signal(self.widget.Inputs.words, words)
+        self.assertFalse(self.widget.Warning.no_words_column.is_shown())
+
+    def test_input_words_no_type(self):
+        words = Table("zoo")
+        self.send_signal(self.widget.Inputs.words, words)
+        self.assertTrue(self.widget.Warning.no_words_column.is_shown())
+        self.send_signal(self.widget.Inputs.words, None)
+        self.assertFalse(self.widget.Warning.no_words_column.is_shown())
+
+
+if __name__ == "__main__":
+    unittest.main()
