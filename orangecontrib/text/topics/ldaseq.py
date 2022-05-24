5708d769df8b59d15882e126669e4e9ecaf689d8
Primoz Godec <p.godec9@gmail.com>
2022-05-24 11:08:30 +0200

    ldaseq: fixes

--- orangecontrib/text/topics/ldaseq.py
+++ orangecontrib/text/topics/ldaseq.py
@@ -5,7 +5,7 @@ from .topics import GensimWrapper
 
 class LdaSeqWrapper(GensimWrapper):
     name = 'Dynamic Topic Modeling'
-    Model = models.ldaseqmodel
+    Model = models.LdaSeqModel
 
     def __init__(self, **kwargs):
         super().__init__(random_state=0, **kwargs, lda_inference_max_iter=200, passes=5)
