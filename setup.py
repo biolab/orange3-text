5523d0295cdbb44cb5bb165d23b550516876f957
Primoz Godec <p.godec9@gmail.com>
2022-06-20 11:57:31 +0200

    Bump version to 1.9.0-dev

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ except ImportError:
 NAME = 'Orange3-Text'
 
 MAJOR = 1
-MINOR = 8
-MICRO = 1
-IS_RELEASED = True
+MINOR = 9
+MICRO = 0
+IS_RELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 FULL_VERSION = VERSION
 
