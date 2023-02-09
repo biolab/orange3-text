a4cd3f438844524ff2f3b8b6a83cc2a0a4089ec0
PrimozGodec <p.godec9@gmail.com>
2023-02-09 13:30:58 +0100

    Wikipedia tests - speedup

--- orangecontrib/text/tests/test_wikipedia.py
+++ orangecontrib/text/tests/test_wikipedia.py
@@ -1,8 +1,12 @@
 import unittest
+from types import SimpleNamespace
 from unittest import mock
+from unittest.mock import call, patch
 
-from orangecontrib.text.wikipedia_api import WikipediaAPI, wikipedia
+from wikipedia import DisambiguationError
+
 from orangecontrib.text.corpus import Corpus
+from orangecontrib.text.wikipedia_api import WikipediaAPI, wikipedia
 
 
 class StoppingMock(mock.Mock):
@@ -19,17 +23,42 @@ class StoppingMock(mock.Mock):
             return False
 
 
+def create_page(title):
+    return SimpleNamespace(
+        title=title,
+        content=title + "text",
+        summary=title,
+        url=title,
+        pageid=int(title[-1]),
+        revision_id=1,
+    )
+
+
+ARTICLES = [f"Article {i}" for i in range(1, 11)]
+ARTICLE_TEXTS = [create_page(title) for title in ARTICLES]
+
+
 class WikipediaTests(unittest.TestCase):
-    def test_search(self):
+    @patch(
+        "orangecontrib.text.wikipedia_api.wikipedia.search", return_value=ARTICLES[:2]
+    )
+    @patch("orangecontrib.text.wikipedia_api.wikipedia.page", side_effect=ARTICLE_TEXTS)
+    def test_search(self, _, search_mock):
         on_progress = mock.MagicMock()
 
         api = WikipediaAPI()
 
-        result = api.search('en', ['Clinton'], articles_per_query=2, on_progress=on_progress)
+        result = api.search(
+            "en", ["Clinton"], articles_per_query=2, on_progress=on_progress
+        )
+        search_mock.assert_called_with("Clinton", results=2)
         self.assertIsInstance(result, Corpus)
         self.assertEqual(len(result.domain.attributes), 0)
         self.assertEqual(len(result.domain.metas), 7)
         self.assertEqual(len(result), 2)
+        self.assertListEqual(
+            [["Article 1"], ["Article 2"]], result[:, "Title"].metas.tolist()
+        )
 
         self.assertEqual(on_progress.call_count, 2)
         progress = 0
@@ -37,14 +66,39 @@ class WikipediaTests(unittest.TestCase):
             self.assertGreater(arg[0][0], progress)
             progress = arg[0][0]
 
-    def test_search_disambiguation(self):
+    @patch(
+        "orangecontrib.text.wikipedia_api.wikipedia.search",
+        side_effect=[ARTICLES[:3], [ARTICLES[4]]],
+    )
+    @patch(
+        "orangecontrib.text.wikipedia_api.wikipedia.page",
+        side_effect=[DisambiguationError("Article 0", "1")] + ARTICLE_TEXTS,
+    )
+    def test_search_disambiguation(self, _, search_mock):
         api = WikipediaAPI()
-        result = api.search('en', ['Scarf'], articles_per_query=3)
+        result = api.search("en", ["Scarf"], articles_per_query=3)
+        search_mock.assert_has_calls((call("Scarf", results=3), call("Article 1", 10)))
 
         self.assertIsInstance(result, Corpus)
         self.assertGreaterEqual(len(result), 3)
 
-    def test_search_break(self):
+    @patch(
+        "orangecontrib.text.wikipedia_api.wikipedia.search",
+        side_effect=[
+            ARTICLES[:2],  # clinton search
+            ARTICLES[2:5],  # first scarf
+            ARTICLES[2:3],  # firs scarf after DisambiguationError
+            ARTICLES[5:8],  # second scarf
+            ARTICLES[5:6],  # second scarf after DisambiguationError
+        ],
+    )
+    @patch(
+        "orangecontrib.text.wikipedia_api.wikipedia.page",
+        side_effect=[DisambiguationError("Article 2", "1")]  # firs scarf
+        + ARTICLE_TEXTS[2:5]  # firs scarf - successes
+        + [DisambiguationError("Article 2", "1")],  # second scarf
+    )
+    def test_search_break(self, _, search_mock):
         api = WikipediaAPI()
 
         # stop immediately
@@ -53,16 +107,27 @@ class WikipediaTests(unittest.TestCase):
         self.assertEqual(len(result), 0)
 
         # stop inside recursion
-        result_all = api.search('en', ['Scarf'], articles_per_query=3)
-        result_stopped = api.search('en', ['Scarf'], articles_per_query=3,
-                                    should_break=StoppingMock(allow_calls=1))
-        self.assertLess(len(result_stopped), len(result_all))
+        r = api.search("en", ["Scarf"], articles_per_query=3)
+        self.assertListEqual(
+            [["Article 3"], ["Article 4"], ["Article 5"]], r[:, "Title"].metas.tolist()
+        )
+        result = api.search(
+            "en",
+            ["Scarf"],
+            articles_per_query=3,
+            should_break=StoppingMock(allow_calls=1),
+        )
+        self.assertEqual(len(result), 0)
 
     def page(*args, **kwargs):
         raise wikipedia.exceptions.PageError('1')
 
     @mock.patch('wikipedia.page', page)
-    def test_page_error(self):
+    @patch(
+        "orangecontrib.text.wikipedia_api.wikipedia.search",
+        return_values=ARTICLES[:3],
+    )
+    def test_page_error(self, _):
         on_error = mock.MagicMock()
         api = WikipediaAPI(on_error=on_error)
         api.search('en', ['Barack Obama'])
@@ -77,3 +142,7 @@ class WikipediaTests(unittest.TestCase):
         api = WikipediaAPI(on_error=on_error)
         api.search('en', ['Barack Obama'])
         self.assertEqual(on_error.call_count, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
