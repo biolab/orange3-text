832d5f1278a94594cb4ca177e2ed06a39e4b2157
PrimozGodec <p.godec9@gmail.com>
2023-04-14 15:03:46 +0200

    Pos - remove unsupported and unused StanfordPOSTagger

--- orangecontrib/text/tag/pos.py
+++ orangecontrib/text/tag/pos.py
@@ -2,7 +2,6 @@ from typing import List, Callable
 
 import nltk
 import numpy as np
-
 from Orange.util import wrap_callback, dummy_callback
 
 from orangecontrib.text import Corpus
@@ -11,7 +10,7 @@ from orangecontrib.text.preprocess import TokenizedPreprocessor
 from orangecontrib.text.util import chunkable
 
 
-__all__ = ['POSTagger', 'StanfordPOSTagger', 'AveragedPerceptronTagger', 'MaxEntTagger']
+__all__ = ["POSTagger", "AveragedPerceptronTagger", "MaxEntTagger"]
 
 
 class POSTagger(TokenizedPreprocessor):
@@ -36,41 +35,6 @@ class POSTagger(TokenizedPreprocessor):
     def _preprocess(self, tokens: List[List[str]]) -> List[List[str]]:
         return list(map(lambda sent: list(map(lambda x: x[1], sent)),
                         self.tagger(tokens)))
-
-
-class StanfordPOSTaggerError(Exception):
-    pass
-
-
-class StanfordPOSTagger(nltk.StanfordPOSTagger, POSTagger):
-    name = 'Stanford POS Tagger'
-
-    @classmethod
-    def check(cls, path_to_model, path_to_jar):
-        """ Checks whether provided `path_to_model` and `path_to_jar` are valid.
-
-        Raises:
-            ValueError: in case at least one of the paths is invalid.
-
-        Notes:
-            Can raise an exception if Java Development Kit is not installed or not properly configured.
-
-        Examples:
-            >>> try:
-            ...    StanfordPOSTagger.check('path/to/model', 'path/to/stanford.jar')
-            ... except ValueError as e:
-            ...    print(e)
-            Could not find stanford-postagger.jar jar file at path/to/stanford.jar
-
-        """
-        try:
-            cls(path_to_model, path_to_jar).tag(())
-        except OSError as e:
-            raise StanfordPOSTaggerError(
-                'Either Java SDK not installed or some of the '
-                'files are invalid.\n' + str(e))
-        except LookupError as e:
-            raise StanfordPOSTaggerError(str(e).strip(' =\n'))
 
 
 class AveragedPerceptronTagger(POSTagger):
