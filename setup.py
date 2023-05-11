94a114c5b0599ed5a4fa8f75de0747d1808c700b
PrimozGodec <p.godec9@gmail.com>
2023-05-11 08:56:22 +0200

    Release version 1.13.1

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ except ImportError:
 NAME = 'Orange3-Text'
 
 MAJOR = 1
-MINOR = 14
-MICRO = 0
-IS_RELEASED = False
+MINOR = 13
+MICRO = 1
+IS_RELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 FULL_VERSION = VERSION
 
